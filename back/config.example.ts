238c5b0c17760bbcd5bfe3807380edf20fafce80
Juanfran <juanfran.alcantara@kaleidos.net>
2019-12-16 08:14:33 +0100

    initial commit

---
+++ back/config.example.ts
@@ -0,0 +1,9 @@
+export default {
+  db: {
+    database: 'mydb',
+    host: '127.0.0.1',
+    password: '123',
+    port: 5432,
+    user: 'user1',
+  },
+};
---
+++ back/src/server.ts
@@ -0,0 +1,37 @@
+import cors from '@koa/cors';
+import Router from '@koa/router';
+import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
+import logger from 'koa-logger';
+import { Pool } from 'pg';
+
+import Config from './config';
+
+const app = new Koa();
+const router = new Router();
+
+const pool = new Pool({
+  database: Config.db.database,
+  host: Config.db.host,
+  password: Config.db.password,
+  port: Config.db.port,
+  user: Config.db.user,
+});
+
+
+
+router.get('/', async (ctx) => {
+  const result = await pool.query('SELECT NOW()');
+
+  ctx.body = {
+    text: `Hello World ${result.rows[0].now}`,
+  };
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .use(cors())
+  .use(bodyParser())
+  .use(logger())
+  .listen(3000);
