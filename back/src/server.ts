4e638a3f0a8fc2bd93dfa90df90a9c373f500eb4
marina.lopez <marina.lopez@kaleidos.net>
2019-12-18 08:57:27 +0100

    wip

--- back/src/server.ts
+++ back/src/server.ts
@@ -32,7 +32,7 @@ router.post('/mock/create', koaBody(), async (ctx) => {
       description: 'plan descripcion',  
       date: '2019-12-09 23:00:00.000Z',
       time: '8:52',    
-      min_attendees: 0
+      min_people: 0
     }
   };
 });
@@ -68,11 +68,10 @@ router.post('/register', koaBody(), async (ctx) => {
     let res;
     const body = ctx.request.body;
     const owner_values = [body.name, body.phone];
-
-    if(isPhoneInAttendees(body.phone)) {
-      res = await pool.query('UPDATE attendee SET name = $1 WHERE phone = $2', owner_values);
+    if(await isPhoneInPersonTable(body.phone)) {
+      res = await pool.query('UPDATE person SET name = $1 WHERE phone = $2', owner_values);
     } else {
-      const owner_text = 'INSERT INTO attendee(name, phone) VALUES($1, $2) RETURNING *';
+      const owner_text = 'INSERT INTO person(name, phone) VALUES($1, $2) RETURNING *';
       res = await pool.query(owner_text, owner_values);
   
       ctx.status = 201;
@@ -99,36 +98,27 @@ router.post('/create/plan', koaBody(), async (ctx) => {
     const body = ctx.request.body
 
     // insert plan
-    const plan_text = 'INSERT INTO plan(name, description, date, time, min_attendees, owner_phone) VALUES($1, $2, $3, $4, $5, $6) RETURNING id';
-    const plan_values = [body.name, body.description, body.date, body.time, body.min_attendees, body.owner_phone];
+    const plan_text = 'INSERT INTO plan(name, description, date, time, min_people, owner_phone) VALUES($1, $2, $3, $4, $5, $6) RETURNING id';
+    const plan_values = [body.name, body.description, body.date, body.time, body.min_people, body.owner_phone];
 
     const createdPlan = await pool.query(plan_text, plan_values);
-    console.log('createdPlannnnnn------>', createdPlan);
+    console.log('createdPlannnnnnn ID------>', createdPlan.rows[0].id);
 
     // set owner answer
-    await pool.query('INSERT INTO answer(plan_id, attendee_phone) VALUES($1, $2)', [createdPlan.rows[0].id, body.owner_phone]);
+    await pool.query('INSERT INTO plan_person(plan_id, person_phone, answer) VALUES($1, $2, $3)', [createdPlan.rows[0].id, body.owner_phone, true]);
 
-    //attendees
-    body.attendees.forEach( async (attendee: any) => {
-      // insert every attendee to plan_attendee
-      await pool.query('INSERT INTO plan_attendee(plan_id, attendee_phone) VALUES($1, $2)', [createdPlan.rows[0].id, attendee]);
+    //person
+    body.people.forEach( async (person: any) => {
+      // insert every person to plan_person
+      await pool.query('INSERT INTO plan_person(plan_id, person_phone, required_person) VALUES($1, $2)', [createdPlan.rows[0].id, person.phone, person.required]);
 
-      // insert every attendee to attendee if phone is not registered
-      if (!isPhoneInAttendees(attendee)) {
-        const text = 'INSERT INTO attendee(phone) VALUES($1)';
-        await pool.query(text, [attendee]);
+      // insert every person to person table if phone is not registered
+      // !!!!!!!!!!!!!!
+      if (!await isPhoneInPersonTable(person)) {
+        const text = 'INSERT INTO person(phone) VALUES($1)';
+        await pool.query(text, [person]);
       }
-
-      // insert answers
-      await pool.query('INSERT INTO answer(plan_id, attendee_phone) VALUES($1, $2)', [createdPlan.rows[0].id, attendee]);
     });
-
-    // insert mandatory attendees
-    if (body.mandatory) {
-      body.mandatory.forEach( async (attendee: any) => {
-        await pool.query('INSERT INTO required_attendee(plan_id, attendee_phone) VALUES($1, $2)', [createdPlan.rows[0].id, attendee]);
-      });
-    }
   } catch(error) {
     ctx.body = {
       status: 'error',
@@ -140,10 +130,19 @@ router.post('/create/plan', koaBody(), async (ctx) => {
 });
 
 router.get('/plans', async (ctx) => {
-  const result = await pool.query('SELECT * FROM plan');
-  ctx.body = {
-    data: {
-      result: result.rows[0]
+  try {
+    const result = await pool.query('SELECT * FROM plan');
+    ctx.body = {
+      data: {
+        result: result.rows[0]
+      }
+    }
+  } catch(error) {
+    ctx.body = {
+      status: 'error',
+      data: {
+        response: error.detail
+      }
     }
   }
 });
@@ -178,8 +177,8 @@ app
   .use(logger())
   .listen(3000);
 
-  async function isPhoneInAttendees(phone: string) {
-    const getPhone = await pool.query('SELECT * FROM attendee WHERE phone = $1', [phone]);
+  async function isPhoneInPersonTable(phone: string) {
+    const getPhone = await pool.query('SELECT * FROM person WHERE phone = $1', [phone]);
   
     return !!(getPhone.rowCount > 0);
   }
--- back/src/tables.sql
+++ back/src/tables.sql
@@ -1,40 +1,25 @@
-CREATE TABLE IF NOT EXISTS attendee (
+CREATE TABLE IF NOT EXISTS person (
   name VARCHAR(45),
   phone VARCHAR(14) NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE IF NOT EXISTS plan (
-  id integer PRIMARY KEY NOT NULL,
+  id BIGSERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(45) NOT NULL,  
   description VARCHAR(450),  
   date Date NOT NULL DEFAULT NOW(),  
   time TIME,
-  min_attendees INTEGER,
+  min_people INTEGER,
   owner_phone VARCHAR(14),
-  FOREIGN KEY (owner_phone) REFERENCES attendee(phone)
+  FOREIGN KEY (owner_phone) REFERENCES person(phone)
 );
 
-CREATE TABLE IF NOT EXISTS plan_attendee (
+CREATE TABLE IF NOT EXISTS plan_person (
   plan_id INTEGER,
-  attendee_phone VARCHAR(14),
-  FOREIGN KEY (plan_id) REFERENCES plan(id) ON UPDATE CASCADE ON DELETE CASCADE,
-  FOREIGN KEY (attendee_phone) REFERENCES attendee(phone) ON UPDATE CASCADE ON DELETE CASCADE,
-  CONSTRAINT plan_attendee_key PRIMARY KEY (plan_id, attendee_phone)
-);
-
-CREATE TABLE IF NOT EXISTS required_attendee (
-  plan_id INTEGER,
-  attendee_phone VARCHAR(14),
-  FOREIGN KEY (plan_id) REFERENCES plan(id) ON UPDATE CASCADE ON DELETE CASCADE,
-  FOREIGN KEY (attendee_phone) REFERENCES attendee(phone) ON UPDATE CASCADE ON DELETE CASCADE,
-  CONSTRAINT required_attendee_key PRIMARY KEY (plan_id, attendee_phone)
-);
-
-CREATE TABLE IF NOT EXISTS answer (
-  plan_id INTEGER,
-  attendee_phone VARCHAR(14),
+  person_phone VARCHAR(14),
+  required_person boolean DEFAULT false,
   answer boolean,
   FOREIGN KEY (plan_id) REFERENCES plan(id) ON UPDATE CASCADE ON DELETE CASCADE,
-  FOREIGN KEY (attendee_phone) REFERENCES attendee(phone) ON UPDATE CASCADE ON DELETE CASCADE,
-  CONSTRAINT answer_key PRIMARY KEY (plan_id, attendee_phone)
+  FOREIGN KEY (person_phone) REFERENCES person(phone) ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT plan_person_key PRIMARY KEY (plan_id, person_phone)
 );
